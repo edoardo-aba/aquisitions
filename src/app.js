f72945bd9ceae2bfc7c636730fc2c051e223da92
edoardo-aba <edoardoababei2003@gmail.com>
2026-01-12 13:07:48 +0100

    Added middlewares

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import  express from "express"
+import logger from '#config/logger.js';
+import helmet from "helmet";
+import morgan from "morgan"
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
 const  PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send("Hello from aquisitions");
-})
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
+app.use(morgan('combined', {stream: { write: (message) => logger.info(message.trim())}}));
+
+app.get('/', (req, res) =>{
+    logger.info('Hello from acquisition');
+    res.status(200).send('Hello from acquisitions!');
+})
 export default  app;
 
 
